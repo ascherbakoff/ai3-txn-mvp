ab09a514696d9764c3cd39dd4ece52ba78adb18c
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2022-09-12 19:06:46 +0300

    hash non unique index

--- src/test/java/com/ascherbakoff/ai3/table/MVStoreWithHashUniqueIndexTest.java
+++ src/test/java/com/ascherbakoff/ai3/table/MVStoreWithHashUniqueIndexTest.java
@@ -3,6 +3,7 @@ package com.ascherbakoff.ai3.table;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import com.ascherbakoff.ai3.clock.Timestamp;
 import com.ascherbakoff.ai3.lock.DeadlockPrevention;
@@ -71,5 +72,32 @@ public class MVStoreWithHashUniqueIndexTest extends MVStoreBasicTest {
         // TX3: id2 = insert [bill, 100], TX3
         var err = assertThrows(CompletionException.class, () -> store.insert(Tuple.create(0, "val3"), txId4).join());
         assertEquals(UniqueException.class, err.getCause().getClass());
+    }
+
+    @Test
+    public void testUniqueWithHistory3() {
+        UUID txId = new UUID(0, 0);
+        UUID txId2 = new UUID(0, 1);
+        UUID txId3 = new UUID(0, 2);
+        UUID txId4 = new UUID(0, 4);
+
+        VersionChain<Tuple> rowId = store.insert(Tuple.create(0, "val0"), txId).join();
+        store.commit(txId, Timestamp.now());
+
+        store.update(rowId, Tuple.create(1, "val1"), txId2).join();
+        store.commit(txId2, Timestamp.now());
+
+        store.insert(Tuple.create(0, "val3"), txId4).join();
+
+//        var err = assertThrows(CompletionException.class, () -> store.insert(Tuple.create(0, "val3"), txId4).join());
+//        assertEquals(UniqueException.class, err.getCause().getClass());
+
+        store.update(rowId, Tuple.create(0, "val2"), txId3).join();
+        store.commit(txId3, Timestamp.now());
+
+        store.commit(txId4, Timestamp.now());
+
+        // TODO FIXME unique violation.
+        fail();
     }
 }
