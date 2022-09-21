c147ad0cb37931128a8dfec4149a442904ac06b2
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2022-09-21 17:56:11 +0300

    sorted index - wip

--- src/test/java/com/ascherbakoff/ai3/table/MVStoreWithUniqueIndexBasicTest.java
+++ src/test/java/com/ascherbakoff/ai3/table/MVStoreWithUniqueIndexBasicTest.java
@@ -223,13 +223,13 @@ public abstract class MVStoreWithUniqueIndexBasicTest extends MVStoreBasicTest {
 
         CompletableFuture<VersionChain<Tuple>> fut = store.insert(Tuple.create(0, "val1"), txId3);
         assertFalse(fut.isDone());
-//        CompletableFuture<Tuple> fut2 = getSingleAsync(txId3, 0, Tuple.create(0));
-//        assertFalse(fut2.isDone());
+        CompletableFuture<Tuple> fut2 = getSingleAsync(txId3, 0, Tuple.create(0));
+        assertFalse(fut2.isDone());
 
         store.commit(txId2, Timestamp.now());
 
         fut.join();
-//        fut2.join();
+        fut2.join();
         assertEquals(Tuple.create(0, "val1"), getSingle(txId3, 0, Tuple.create(0)));
     }
 
@@ -253,7 +253,7 @@ public abstract class MVStoreWithUniqueIndexBasicTest extends MVStoreBasicTest {
         store.abort(txId2);
 
         assertEquals(Tuple.create(0, "val0"), fut2.join());
-//        var err = assertThrows(CompletionException.class, () -> fut.join());
-//        assertEquals(UniqueException.class, err.getCause().getClass());
+        var err = assertThrows(CompletionException.class, () -> fut.join());
+        assertEquals(UniqueException.class, err.getCause().getClass());
     }
 }
